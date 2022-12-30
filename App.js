8c6b600852022e2b6c2056db4f338eb920b1d63a
YuNiwa <yniwa0128@gmail.com>
2022-12-30 20:12:16 +0900

    get通信、ログイン機能の改良

--- App.js
+++ App.js
@@ -1,31 +1,40 @@
-import React from 'react';
+import React, { Component, useState } from 'react';
 import {StyleSheet, View, Text, Button, TextInput, SafeAreaView} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import App2 from './App2';
+import axios from "axios";
+const baseURL = "https://facebook.github.io/react-native/movies.json";
+let this_;
 const Stack = createStackNavigator()
-const App = ({navigation}) => {
-  return (
-    <NavigationContainer>
+let navigation_;
 
-    <Stack.Navigator screenOptions={{
-        headerShown: false
-      }} initialRouteName="Details">
-      <Stack.Screen 
-        name="Details" 
-        component={DetailsScreen} 
-      />
-      <Stack.Screen 
-        name="Home" 
-        component={App2} 
-      />
+export default class App extends Component<{}> {
+  
+  constructor(props) {
+    super(props);
+    this_ = this;
+  }  
 
-      
-    </Stack.Navigator>
-
-  </NavigationContainer>
-  );
-};
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{
+            headerShown: false
+          }} initialRouteName="Details">
+          <Stack.Screen 
+            name="Details" 
+            component={DetailsScreen} 
+          />
+          <Stack.Screen 
+            name="Home" 
+            component={App2} 
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   input: {
@@ -39,8 +48,15 @@ const styles = StyleSheet.create({
   },
 });
 
+function login_action () {
+  axios.get(baseURL).then((response) => {
+    console.log(response.data);
+    navigation_.navigate('Home');
+  });
+}
 
 function DetailsScreen({ navigation }) {
+  navigation_ = navigation;
   return (
     <SafeAreaView>
       <TextInput
@@ -53,12 +69,10 @@ function DetailsScreen({ navigation }) {
       />
       <Button
         title="ログイン"
-        onPress={() => {
-          navigation.navigate('Home');
-        }}
+        onPress={
+          login_action
+        }
       />
     </SafeAreaView>
   )
 }
-
-export default App;
