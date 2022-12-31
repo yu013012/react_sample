d3e9ec44baab032a3450f7a076fd1911624e9d0e
YuNiwa <yniwa0128@gmail.com>
2022-12-31 10:50:10 +0900

    ログイン画面get通信、エラー表示、画面遷移機能追加

--- App.js
+++ App.js
@@ -4,16 +4,18 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import App2 from './App2';
 import axios from "axios";
-const baseURL = "https://facebook.github.io/react-native/movies.json";
 let this_;
 const Stack = createStackNavigator()
 let navigation_;
-
+let id, setId, pass, setPass, error, setError;
 export default class App extends Component<{}> {
   
   constructor(props) {
     super(props);
     this_ = this;
+    this.state = {
+      text: "dsfs",
+    };
   }  
 
   render() {
@@ -49,22 +51,39 @@ const styles = StyleSheet.create({
 });
 
 function login_action () {
+  const baseURL = "https://www.it-service.co.jp/cgi-local/gosui/gosui_app.pl?ACT=CHECK_TNO&ID="+id+"&PASS="+pass;
   axios.get(baseURL).then((response) => {
-    console.log(response.data);
-    navigation_.navigate('Home');
+    if (response.data.tno) {
+      navigation_.navigate('Home');
+    } else {
+      //エラー表示
+      console.log("エラー表示");
+      setError("正しい情報を入力してください")
+    }
   });
 }
 
 function DetailsScreen({ navigation }) {
   navigation_ = navigation;
+  [id, setId] = useState('');
+  [pass, setPass] = useState('');
+  [error, setError] = useState('');
   return (
     <SafeAreaView>
+      <Text style={styles.text}>
+        { "ログイン画面" }
+      </Text>
+      <Text style={styles.text}>
+        { error }
+      </Text>
       <TextInput
         style={styles.input}
+        onChangeText={id_ => setId(id_)}
         placeholder="ID"
       />
       <TextInput
         style={styles.input}
+        onChangeText={pass_ => setPass(pass_)}
         placeholder="パスワード"
       />
       <Button
