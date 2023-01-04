eb192c8aead0d847c694a94b6b3143c69744420a
丹羽悠 <y_niwa@niwayuunoMacBook-Pro-2.local>
2023-01-04 22:00:17 +0900

    取得した名前、uuidを使ってblue接続するように修正、レイアウトの修正、スタートボタンの追加

--- App2.js
+++ App2.js
@@ -9,6 +9,7 @@ import {
   NativeModules,
   NativeEventEmitter,
   Modal,
+  ScrollView,
   AsyncStorage
 } from 'react-native';
 import Sound from 'react-native-sound';
@@ -22,6 +23,7 @@ const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
 ///cgi-local/gosui/gosui_app.pl?ACT=UPDATE_RECORD&TNO=$inputTno&MNO=$mno&record_direction=$recordDirection&record_date=$recordDate&record_finish_date=$recordFinishDate&body_temperature=$lastBodyTemperature"
 let alert_flg1, alert_flg2, alert_flg3, alert_flg4, alert_flg5, alert_flg6, alert_flg7, alert_flg8, alert_flg9, alert_flg10;
 let uuid1, uuid2, uuid3, uuid4, uuid5, uuid6, uuid7, uuid8, uuid9, uuid10;
+let name1, name2, name3, name4, name5, name6, name7, name8, name9, name10;
 let uuid1_ios, uuid2_ios, uuid3_ios, uuid4_ios, uuid5_ios, uuid6_ios, uuid7_ios, uuid8_ios, uuid9_ios, uuid10_ios;
 let state_this;
 let sound_flg = 0;
@@ -50,6 +52,7 @@ export default class App extends Component<{}> {
       console.log(token);
     });
 
+    // センサーデータ読み込み
     this.loadItem();
 
     // 1.5秒ごとにalert実行
@@ -85,88 +88,53 @@ export default class App extends Component<{}> {
             if (args.id == uuid1) {
               blue_connect(uuid1)
               blue_write(uuid1, uuid1_ios)
-              blue_notification(uuid1, uuid1_ios)
+              blue_notification(uuid1)
             }
-            var uuid = "4C:02:2E:7E:4F:12";
-            if (Platform.OS == 'ios') {
-              uuid = "ED2E8A6E-E9B2-6C15-8BE9-1193AA4CECAD";
+            
+            if (args.id == uuid2) {
+              blue_connect(uuid2)
+              blue_write(uuid2, uuid2_ios)
+              blue_notification(uuid2)
             }
-            if (args.id == uuid) {
-              blue_connect(uuid)
-              blue_write(uuid, "ED2E8A6E-E9B2-6C15-8BE9-1193AA4CECAD")
-              blue_notification(uuid, "ED2E8A6E-E9B2-6C15-8BE9-1193AA4CECAD")
+            if (args.id == uuid3) {
+              blue_connect(uuid3)
+              blue_write(uuid3, uuid3_ios)
+              blue_notification(uuid3)
             }
-            uuid = "4C:02:2E:7E:55:2B";
-            if (Platform.OS == 'ios') {
-              uuid = "D8812D36-9049-EE60-4287-56FA84E99CB0";
+            if (args.id == uuid4) {
+              blue_connect(uuid4)
+              blue_write(uuid4, uuid4_ios)
+              blue_notification(uuid4)
             }
-            if (args.id == uuid) {
-              blue_connect(uuid)
-              blue_write(uuid, "D8812D36-9049-EE60-4287-56FA84E99CB0")
-              blue_notification(uuid, "D8812D36-9049-EE60-4287-56FA84E99CB0")
+            if (args.id == uuid5) {
+              blue_connect(uuid5)
+              blue_write(uuid5, uuid5_ios)
+              blue_notification(uuid5)
             }
-            uuid = "4C:02:2E:7E:55:0A";
-            if (Platform.OS == 'ios') {
-              uuid = "D94920AE-A124-D354-33A8-74C416FDCF7E";
+            if (args.id == uuid6) {
+              blue_connect(uuid6)
+              blue_write(uuid6, uuid6_ios)
+              blue_notification(uuid6)
             }
-            if (args.id == uuid) {
-              blue_connect(uuid)
-              blue_write(uuid, "D94920AE-A124-D354-33A8-74C416FDCF7E")
-              blue_notification(uuid, "D94920AE-A124-D354-33A8-74C416FDCF7E")
+            if (args.id == uuid7) {
+              blue_connect(uuid7)
+              blue_write(uuid7, uuid7_ios)
+              blue_notification(uuid7)
             }
-            uuid = "4C:02:2E:7E:56:CB";
-            if (Platform.OS == 'ios') {
-              uuid = "0A79209D-CD97-80A4-FEB2-586D0E152A9B";
+            if (args.id == uuid8) {
+              blue_connect(uuid8)
+              blue_write(uuid8, uuid8_ios)
+              blue_notification(uuid8)
             }
-            if (args.id == uuid) {
-              blue_connect(uuid)
-              blue_write(uuid, "0A79209D-CD97-80A4-FEB2-586D0E152A9B")
-              blue_notification(uuid, "0A79209D-CD97-80A4-FEB2-586D0E152A9B")
+            if (args.id == uuid9) {
+              blue_connect(uuid9)
+              blue_write(uuid9, uuid9_ios)
+              blue_notification(uuid9)
             }
-            uuid = "4C:02:2E:7E:5D:97";
-            if (Platform.OS == 'ios') {
-              uuid = "354142CC-FC1F-3D4E-1D1F-0F391742C4AD";
-            }
-            if (args.id == uuid) {
-              blue_connect(uuid)
-              blue_write(uuid, "354142CC-FC1F-3D4E-1D1F-0F391742C4AD")
-              blue_notification(uuid, "354142CC-FC1F-3D4E-1D1F-0F391742C4AD")
-            }
-            uuid = "4C:02:2E:7E:57:9F";
-            if (Platform.OS == 'ios') {
-              uuid = "C1372667-C51E-030B-D005-702D82F11356";
-            }
-            if (args.id == uuid) {
-              blue_connect(uuid)
-              blue_write(uuid, "C1372667-C51E-030B-D005-702D82F11356")
-              blue_notification(uuid, "C1372667-C51E-030B-D005-702D82F11356")
-            }
-            uuid = "4C:02:2E:7E:57:22";
-            if (Platform.OS == 'ios') {
-              uuid = "F2500A70-6471-1E29-C47D-3BFAA6AA7C40";
-            }
-            if (args.id == uuid) {
-              blue_connect(uuid)
-              blue_write(uuid, "F2500A70-6471-1E29-C47D-3BFAA6AA7C40")
-              blue_notification(uuid, "F2500A70-6471-1E29-C47D-3BFAA6AA7C40")
-            }
-            uuid = "4C:02:2E:7E:57:0A";
-            if (Platform.OS == 'ios') {
-              uuid = "AC936418-D8B7-D747-A4B0-200FC50D61CF";
-            }
-            if (args.id == uuid) {
-              blue_connect(uuid)
-              blue_write(uuid, "AC936418-D8B7-D747-A4B0-200FC50D61CF")
-              blue_notification(uuid, "AC936418-D8B7-D747-A4B0-200FC50D61CF")
-            }
-            uuid = "4C:02:2E:7E:56:C7";
-            if (Platform.OS == 'ios') {
-              uuid = "F361DD9B-F42D-CCC7-FAC9-F4339B3502E0";
-            }
-            if (args.id == uuid) {
-              blue_connect(uuid)
-              blue_write(uuid, "F361DD9B-F42D-CCC7-FAC9-F4339B3502E0")
-              blue_notification(uuid, "F361DD9B-F42D-CCC7-FAC9-F4339B3502E0")
+            if (args.id == uuid10) {
+              blue_connect(uuid10)
+              blue_write(uuid10, uuid10_ios)
+              blue_notification(uuid10)
             }
           }
         );
@@ -179,44 +147,8 @@ export default class App extends Component<{}> {
       ({ value, peripheral, characteristic, service }) => {
         let test = String(value);
         let monthAry = test.split(',');
-        var uuid = "4C:02:2E:7E:4F:12";
-        if (Platform.OS == 'ios') {
-          uuid = "ED2E8A6E-E9B2-6C15-8BE9-1193AA4CECAD";
-        }
-        var uuid2 = "4C:02:2E:7E:55:2B";
-        if (Platform.OS == 'ios') {
-          uuid2 = "D8812D36-9049-EE60-4287-56FA84E99CB0";
-        }
-        var uuid3 = "4C:02:2E:7E:55:0A";
-        if (Platform.OS == 'ios') {
-          uuid3 = "D94920AE-A124-D354-33A8-74C416FDCF7E";
-        }
-        var uuid4 = "4C:02:2E:7E:56:CB";
-        if (Platform.OS == 'ios') {
-          uuid4 = "0A79209D-CD97-80A4-FEB2-586D0E152A9B";
-        }
-        var uuid5 = "4C:02:2E:7E:5D:97";
-        if (Platform.OS == 'ios') {
-          uuid5 = "354142CC-FC1F-3D4E-1D1F-0F391742C4AD";
-        }
-        var uuid6 = "4C:02:2E:7E:57:9F";
-        if (Platform.OS == 'ios') {
-          uuid6 = "C1372667-C51E-030B-D005-702D82F11356";
-        }
-        var uuid7 = "4C:02:2E:7E:57:22";
-        if (Platform.OS == 'ios') {
-          uuid7 = "F2500A70-6471-1E29-C47D-3BFAA6AA7C40";
-        }
-        var uuid8 = "4C:02:2E:7E:57:0A";
-        if (Platform.OS == 'ios') {
-          uuid8 = "AC936418-D8B7-D747-A4B0-200FC50D61CF";
-        }
-        var uuid9 = "4C:02:2E:7E:56:C7";
-        if (Platform.OS == 'ios') {
-          uuid9 = "F361DD9B-F42D-CCC7-FAC9-F4339B3502E0";
-        }
         if (31 <= monthAry[17] && monthAry[17] <= 65) {
-          if (peripheral == uuid) {
+          if (peripheral == uuid1) {
             alert_flg2 = 1;
             state_this.setState({
               count2: "↓"
@@ -270,7 +202,7 @@ export default class App extends Component<{}> {
         // 仰向けの時(z軸128~223、仰向けMIN191がとれるが時々条件不明で165等が取得できるので128まで見るようにしている)
         } else if (128 <= monthAry[17] && monthAry[17] <= 223) {
           // ↑
-          if (peripheral == uuid) {
+          if (peripheral == uuid1) {
             alert_flg2 = 0;
             state_this.setState({
               count2: "↑"
@@ -325,7 +257,7 @@ export default class App extends Component<{}> {
         } else if (224 <= monthAry[17] && monthAry[17] <= 255 || 0 <= monthAry[17] && monthAry[17] <= 30) {
           // 右向き
           if (224 <= monthAry[17] && monthAry[17] <= 255) {
-            if (peripheral == uuid) {
+            if (peripheral == uuid1) {
               alert_flg2 = 0;
               state_this.setState({
                 count2: "⇨"
@@ -378,7 +310,7 @@ export default class App extends Component<{}> {
             }
           // 左向き
           } else if (0 <= monthAry[17] && monthAry[17] <= 30) {
-            if (peripheral == uuid) {
+            if (peripheral == uuid1) {
               alert_flg2 = 0;
               state_this.setState({
                 count2: "⇦"
@@ -433,7 +365,7 @@ export default class App extends Component<{}> {
         } else {
           
           /* 72 ~ 127 : シェイク判定??*/
-          if (peripheral == uuid) {
+          if (peripheral == uuid1) {
             alert_flg2 = 0;
             state_this.setState({
               count2: "↑"
@@ -488,43 +420,155 @@ export default class App extends Component<{}> {
       });
     });
   }
-  
+
+  start1() {
+    console.log("1");
+  }
+
+  start2() {
+    console.log("2");
+  }
+
+  start3() {
+    console.log("3");
+  }
+
+  start4() {
+    console.log("4");
+  }
+
+  start5() {
+    console.log("5");
+  }
+
+  start6() {
+    console.log("6");
+  }
+
+  start7() {
+    console.log("7");
+  }
+
+  start8() {
+    console.log("8");
+  }
+
+  start9() {
+    console.log("9");
+  }
+
+  start10() {
+    console.log("10");
+  }
+
   render() {
     return (
         <View style={styles.container}>
-          <Text style={styles.text}>
-            トークン { token }
-          </Text>
-          <Text style={styles.text}>
-          NO.1 山田 { this.state.count }
-          </Text>
-          <Text style={styles.text}>
-          NO.2 佐藤 { this.state.count2 }
-          </Text>
-          <Text style={styles.text}>
-          NO.3 丹羽 { this.state.count3 }
-          </Text>
-          <Text style={styles.text}>
-          NO.4 高橋 { this.state.count4 }
-          </Text>
-          <Text style={styles.text}>
-          NO.5 坂本 { this.state.count5 }
-          </Text>
-          <Text style={styles.text}>
-          NO.6 鈴木 { this.state.count6 }
-          </Text>
-          <Text style={styles.text}>
-          NO.7 花子 { this.state.count7 }
-          </Text>
-          <Text style={styles.text}>
-          NO.8 太郎 { this.state.count8 }
-          </Text>
-          <Text style={styles.text}>
-          NO.9 三郎 { this.state.count9 }
-          </Text>
-          <Text style={styles.text}>
-          NO.10 史郎 { this.state.count10 }
-          </Text>
+          <ScrollView>
+            <Text style={styles.text}>
+              トークン { token }
+            </Text>
+            <View style={styles.button}>
+              <Button
+                onPress={this.start1}
+                title="午睡チェック開始"
+                color="#000004"
+              />
+            </View>
+            <Text style={styles.text}>
+              NO.1 { name1 } { this.state.count }
+            </Text>
+            <View style={styles.button}>
+              <Button
+                onPress={this.start2}
+                title="午睡チェック開始"
+                color="#000004"
+              />
+            </View>
+            <Text style={styles.text}>
+              NO.2 { name2 } { this.state.count2 }
+            </Text>
+            <View style={styles.button}>
+              <Button
+                onPress={this.start3}
+                title="午睡チェック開始"
+                color="#000004"
+              />
+            </View>
+            <Text style={styles.text}>
+              NO.3 { name3 } { this.state.count3 }
+            </Text>
+            <View style={styles.button}>
+              <Button
+                onPress={this.start4}
+                title="午睡チェック開始"
+                color="#000004"
+              />
+            </View>
+            <Text style={styles.text}>
+              NO.4 { name4 } { this.state.count4 }
+            </Text>
+            <View style={styles.button}>
+              <Button
+                onPress={this.start5}
+                title="午睡チェック開始"
+                color="#000004"
+              />
+            </View>
+            <Text style={styles.text}>
+              NO.5 { name5 } { this.state.count5 }
+            </Text>
+            <View style={styles.button}>
+              <Button
+                onPress={this.start6}
+                title="午睡チェック開始"
+                color="#000004"
+              />
+            </View>
+            <Text style={styles.text}>
+              NO.6 { name6 } { this.state.count6 }
+            </Text>
+            <View style={styles.button}>
+              <Button
+                onPress={this.start7}
+                title="午睡チェック開始"
+                color="#000004"
+              />
+            </View>
+            <Text style={styles.text}>
+              NO.7 { name7 } { this.state.count7 }
+            </Text>
+            <View style={styles.button}>
+              <Button
+                onPress={this.start8}
+                title="午睡チェック開始"
+                color="#000004"
+              />
+            </View>
+            <Text style={styles.text}>
+              NO.8 { name8 } { this.state.count8 }
+            </Text>
+            <View style={styles.button}>
+              <Button
+                onPress={this.start9}
+                title="午睡チェック開始"
+                color="#000004"
+              />
+            </View>
+            <Text style={styles.text}>
+              NO.9 { name9 } { this.state.count9 }
+            </Text>
+            <View style={styles.button}>
+              <Button
+                onPress={this.start10}
+                title="午睡チェック開始"
+                color="#000004"
+              />
+            </View>
+            <Text style={styles.text}>
+              NO.10 { name10 } { this.state.count10 }
+            </Text>
+          </ScrollView>
           <Modal visible={this.state.modalVisible}>
             <View style={styles.container}>
               <Text style={styles.text}>
@@ -541,7 +585,7 @@ export default class App extends Component<{}> {
     try {
       const tno = await AsyncStorage.getItem("tno");
       if(tno) {
-        const baseURL = "https://www.cloudtest2.pw/cgi-local/gosui/gosui_app.pl?ACT=GET_DATA&TNO="+tno+"&token="+token;
+        const baseURL = "https://www.cloudtest2.pw/cgi-local/gosui/gosui_app.pl?ACT=GET_DATA&TNO="+tno+"&token=cb4114e454732c67";//+token;
         axios.get(baseURL).then((response) => {
           if (response) {
             if (Platform.OS == 'ios') {
@@ -577,6 +621,17 @@ export default class App extends Component<{}> {
             uuid8_ios = response.data.uuid8;
             uuid9_ios = response.data.uuid9;
             uuid10_ios = response.data.uuid10;
+
+            name1 = response.data.name1;
+            name2 = response.data.name2;
+            name3 = response.data.name3;
+            name4 = response.data.name4;
+            name5 = response.data.name5;
+            name6 = response.data.name6;
+            name7 = response.data.name7;
+            name8 = response.data.name8;
+            name9 = response.data.name9;
+            name10 = response.data.name10;
           } else {
             //エラー表示
             console.log("エラー表示");
@@ -628,7 +683,7 @@ function blue_write(uuid, ios_uuid) {
   });
 }
 
-function blue_notification(uuid, ios_uuid) {
+function blue_notification(uuid) {
   BleManager.startNotification(
     uuid,
     "0000C62E-9910-0BAC-5241-D8BDA6932A2F",
@@ -649,5 +704,11 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 25,
+  },
+  button: {
+    backgroundColor: "pink",
+  },
+  sview: {
+    alignItems: 'center',
   },
 });
